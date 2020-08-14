dc6dbcb59788cdfa93f0d57bf739508af3c6db0b
Fabio Andrade <fmandrade2319@gmail.com>
2020-08-14 16:04:05 -0300

    Revert "Adiciona o modulo load-express, porem sem sucesso!"
    This reverts commit f1b96651b94c16cbfb62847b1164faf497d5fbed.

--- config/express.js
+++ config/express.js
@@ -1,6 +1,5 @@
 var express = require('express');
-//var home = require('../app/routes/home'); //importa o módulo home de routes//substituído pelo "load"
-var load = require('express-load'); //importa o módulo load (seção 2.9 pág30)
+var home = require('../app/routes/home'); //importa o módulo home de routes
 
 module.exports = function() {
     var app = express();
@@ -14,12 +13,7 @@ module.exports = function() {
     app.set('view engine','ejs'); //define o ejs como template engine
     app.set('views','./app/views'); //define o diretório onde ficarão as views, assim a pagina index.html será movida para o diretório views e sua extensão alterada para .ejs
 
-    //home(app); substituída pela chamada ao load abaixo
-
-    load('models', {cwd: 'app'})
-        .then('controllers')
-        .then('routes')
-        .into(app);
+    home(app);
 
     return app;
 };
